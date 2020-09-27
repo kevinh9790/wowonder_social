2d7e65e17c454158a16deffc5db73494e4196bdb
heke0621 <heke0621@gmail.com>
2020-09-27 16:01:34 +0800

    代管資料庫完成

--- Script/config.php
+++ Script/config.php
@@ -9,16 +9,16 @@
 // | Copyright (c) 2016 WoWonder. All rights reserved.
 // +------------------------------------------------------------------------+
 // MySQL Hostname
-$sql_db_host = "localhost";
+$sql_db_host = "db4free.net";
 // MySQL Database User
-$sql_db_user = "root";
+$sql_db_user = "heke0621";
 // MySQL Database Password
-$sql_db_pass = "";
+$sql_db_pass = "7UHTCY6@876";
 // MySQL Database Name
-$sql_db_name = "wowonder";
+$sql_db_name = "heke0621_music";
 
 // Site URL
-$site_url = "http://localhost/wowonder_social/Script"; // e.g (http://example.com)
+$site_url = "https://localhost/wowonder_social/Script"; // e.g (http://example.com)
 
 // Purchase code
 $purchase_code = "heke0621"; // Your purchase code, don't give it to anyone. 
